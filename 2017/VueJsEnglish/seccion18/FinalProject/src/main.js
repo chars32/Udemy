8b6519a1e634007da2a7603408a87c7e2f4296d6
Carlos Garcia <chars24@gmail.com>
2017-06-10 23:36:19 -0500

    3. Seteando las rutas (routes) del proyecto

--- 2017/VueJsEnglish/seccion18/FinalProject/src/main.js
+++ 2017/VueJsEnglish/seccion18/FinalProject/src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import { routes } from './route.js'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history', // history evita ver urls feas 
+  routes: routes // tambien podria ser solo routes por la sintaxis ES6
+});
 
 new Vue({
   el: '#app',
+  router, //aqui si usamos la ventaja que da ES6 de no escribir lo mismo 2 veces (router: router)
   render: h => h(App)
 })
